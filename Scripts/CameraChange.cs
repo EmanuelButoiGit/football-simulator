9815ac157d06d4628da9258d75e939fcec5fe083
EmanuelButoiGit <emanuelb1000@gmail.com>
2023-08-04 22:06:13 +0300

    Create CameraChange.cs

---
+++ Scripts/CameraChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject cameraFPS;
+    public GameObject cameraFreeView;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            cameraFPS.SetActive(true);
+            cameraFreeView.SetActive(false);
+        }
+        
+        else if (Input.GetKey("2"))
+        {
+            cameraFPS.SetActive(false);
+            cameraFreeView.SetActive(true);
+        }
+    }
+}
